50f2bc2cb69875e0b1c9a6c9b1a9e002c6f353b6
Dave Kilian <dave.kilian@gmail.com>
2017-04-24 20:43:06 -0600

    Main render loop

--- game.ts
+++ game.ts
@@ -1,17 +1,44 @@
 
 class Game {
-    start() {
-        window.requestAnimationFrame(() => this.mainLoop());
+    lastUpdate: number;
+    ctx: CanvasRenderingContext2D;
+    debugTime: 0;
+
+    constructor() {
+        this.lastUpdate = 0;
+        this.ctx = (<HTMLCanvasElement>document.getElementById('physgame')).getContext('2d');
+        this.debugTime = 0;
     }
 
-    private renderTest() {
-        var c = (<HTMLCanvasElement>document.getElementById('physgame')).getContext('2d');
+    private mainLoop(time: number) {
+        const FPS = 60;
+
+        if (time - this.lastUpdate > 1000 / FPS) {
+            let delta = (time - this.lastUpdate) / 1000;
+
+            this.update(delta);   
+            this.render(delta);
+
+            this.lastUpdate = time;
+        }
+
+        window.requestAnimationFrame((t) => this.mainLoop(t));
+    }
+
+    private update(t: number) {
+        this.debugTime += t;
+    }
+
+    private render(t: number) {
+        let c = this.ctx;
         c.clearRect(0, 0, c.canvas.width, c.canvas.height);
-        c.fillStyle = "black";
+
+        let color = Math.floor(255 * (.5 + .5 * Math.sin(this.debugTime)));
+        c.fillStyle = `rgb(${color}, 0, 0)`;
         c.fillRect(0, 0, c.canvas.width, c.canvas.height);
     }
 
-    private mainLoop() {
-        this.renderTest();
+    start() {
+        window.requestAnimationFrame((t) => this.mainLoop(t));
     }
 }
