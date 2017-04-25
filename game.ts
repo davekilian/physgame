7428efeb934c72fe9cd1ee5e6ebdee922153a612
Dave Kilian <dave.kilian@gmail.com>
2017-04-24 19:43:05 -0600

    Canvas hello world

--- game.ts
+++ game.ts
@@ -1,1 +1,9 @@
-document.body.innerHTML = "Hello world!";
+
+function physgame_main() {
+    var c = (<HTMLCanvasElement>document.getElementById('physgame')).getContext('2d');
+
+    c.clearRect(0, 0, c.canvas.width, c.canvas.height);
+
+    c.fillStyle = "black";
+    c.fillRect(0, 0, c.canvas.width, c.canvas.height);
+}
